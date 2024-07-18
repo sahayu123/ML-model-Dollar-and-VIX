c8b8fee964f37e1db4e4759648fc4fc79ccf9c4e
Raja Subramanian <sahayu.raja@gmail.com>
2024-07-18 14:34:17 -0700

    Second Commit

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
-print("Initial Commit")
+import pandas as pd 
+dollar_df = pd.read_csv("dollar.csv")
+vix_df = pd.read_csv("vix.csv")
+
+# Dropping the Volume Data Column, NAN values, and duplicate values 
+def clean_data(df):
+    df= df.drop(columns=["Volume"])
+    df= df.dropna()
+    df= df.drop_duplicates()
+    return df
+
+vix_df=clean_data(vix_df)
+dollar_df=clean_data(dollar_df)
+
+#Adding the Change Column 
+def add_change(df):
+    change_list=list()
+    for index, row in df.iterrows():
+        try:
+            change_list.append(df["Close"][index]-df["Close"][index-1])
+        except: 
+            change_list.append(0.0)
+    return change_list
+
+vix_df["Change"]=add_change(vix_df)
+print(vix_df)
+
+dollar_df["Change"]=add_change(dollar_df)
+print(dollar_df)
+
+
+
+
+
+
+
+# number of data 
+# clean the NAN values
+# calculate change from  yestersterday, use close values. Plot it. 
+# calculate correlation. Use some sort of correlation metric. Compute correlation. Group up by month or year, and compute correlation. 
+# use 50 days data. If there is daily seasonality, it would help smooth out. 
+# check if there are outlier data 
+# check when VIX or US dollar index are abnormally high. 
+# Plot time series data. 
+# filter out everything before VIX index. 
+# create one dataset with everything 
+
+'''
+High priority: comoputing total data amount
+(number of rows and columns in each csv) 
+Computing the number of missing values and duplicate entries
+Moderate priority: making a histogram of the values of usdx and vix
+Low priority: plot over time of vix and usdx
+outlier analysis
+(when are things the highest/lowest)
+Grouping by day and computing correlations
+Computing the lagged differences and plotting histogram
+'''
