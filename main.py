3e186ea0bcfad0e94f8628f1e86554ce5b603623
Raja Subramanian <sahayu.raja@gmail.com>
2024-07-19 18:27:49 -0700

    Dollar is in VIX range

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def clean_data(df):
 vix_df=clean_data(vix_df)
 dollar_df=clean_data(dollar_df)
 
+vix_df=vix_df.reset_index(drop=True)
+dollar_df=dollar_df.reset_index(drop=True)
+
 #Adding the Change Column 
 def add_change(df):
     change_list=list()
@@ -26,8 +29,19 @@ def add_change(df):
 vix_df["Change"]=add_change(vix_df)
 dollar_df["Change"]=add_change(dollar_df)
 
+# Fitting the Dollar in the range of the vix
+def print_start_and_end(df):
+    print(df.loc[df["Date"]=="1990-01-02"])   
+    print(df.loc[df["Date"]=="2024-06-21"])      
+# Start : 4792
+# End : 13581
+
+dollar_df=dollar_df[4792:13582]
+dollar_df=dollar_df.reset_index(drop=True)
+
 print(vix_df)
 print(dollar_df)
+
 
 # Plotting the VIX and Dollar data. X-axis : Dates, Y-axis: Close price
 vix_df.plot(x="Date",y="Close",title="Close VIX data")
@@ -44,26 +58,3 @@ plt.show()
 
 
 
-
-# number of data 
-# clean the NAN values
-# calculate change from  yestersterday, use close values. Plot it. 
-# calculate correlation. Use some sort of correlation metric. Compute correlation. Group up by month or year, and compute correlation. 
-# use 50 days data. If there is daily seasonality, it would help smooth out. 
-# check if there are outlier data 
-# check when VIX or US dollar index are abnormally high. 
-# Plot time series data. 
-# filter out everything before VIX index. 
-# create one dataset with everything 
-
-'''
-High priority: comoputing total data amount
-(number of rows and columns in each csv) 
-Computing the number of missing values and duplicate entries
-Moderate priority: making a histogram of the values of usdx and vix
-Low priority: plot over time of vix and usdx
-outlier analysis
-(when are things the highest/lowest)
-Grouping by day and computing correlations
-Computing the lagged differences and plotting histogram
-'''
